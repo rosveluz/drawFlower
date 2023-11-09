5df36ce59e3a0454f755789c232dbe3d55125827
rosveluz <112733712+rosveluz@users.noreply.github.com>
2023-11-10 01:28:16 +0800

    Aspect ratio fix test

--- capture.js
+++ capture.js
@@ -1,9 +1,5 @@
-/* capture.js */
-
 const modal = document.getElementById("imageModal");
-const modalImage = document.getElementById("modalImage");
 
-// Function to calculate the scale and position to maintain aspect ratio
 function calculateAspectRatioFit(srcWidth, srcHeight, maxWidth, maxHeight) {
     var ratio = Math.min(maxWidth / srcWidth, maxHeight / srcHeight);
     return {
@@ -25,45 +21,15 @@ async function captureCanvasAndBackground() {
     ctx.fillStyle = backgroundColor;
     ctx.fillRect(0, 0, 1080, 1080);
 
-    // Draw the current generated image in the center with correct aspect ratio
+    // Get the actual size of the drawing surface of 'myCanvas'
     const flowerCanvas = document.getElementById('myCanvas');
-    const aspectFit = calculateAspectRatioFit(flowerCanvas.width, flowerCanvas.height, 400, 400);
-    ctx.drawImage(flowerCanvas, (1080 - aspectFit.width) / 2, (1080 - aspectFit.height) / 2, aspectFit.width, aspectFit.height);
+    const realWidth = flowerCanvas.width;
+    const realHeight = flowerCanvas.height;
 
-    // Save the result canvas as an image
-    const finalImage = finalCanvas.toDataURL("image/png");
+    // Calculate aspect ratio fit based on actual size of 'myCanvas'
+    const aspectFit = calculateAspectRatioFit(realWidth, realHeight, finalCanvas.width, finalCanvas.height);
+    ctx.drawImage(flowerCanvas, aspectFit.offsetX, aspectFit.offsetY, aspectFit.width, aspectFit.height);
 
-    // Set the captured image source to the modal and show the modal
-    modalImage.src = finalImage;
-    modal.style.display = "flex";
+    // Here, consider what you want to do with 'finalCanvas' after capture
+    // For example, appending it to the document, saving it, etc.
 }
-
-function setupCaptureTimer() {
-    const timerDiv = document.getElementById('timer');
-    const buttonText = document.getElementById('button-text');
-    let counter = 5;  // 5 seconds
-
-    // Temporarily clear the button text while the timer is running
-    buttonText.textContent = '';
-
-    const interval = setInterval(async () => { // make this async to allow await inside
-        if (counter <= 0) {
-            clearInterval(interval);
-            buttonText.textContent = 'Capture Timer'; // Reset to original text
-            timerDiv.textContent = ''; // Clear the timer display
-            await captureCanvasAndBackground(); // this will display the modal
-            return;
-        }
-
-        // Display the counter in the timerDiv, effectively replacing the buttonText
-        timerDiv.textContent = `Capture in ${counter}...`;
-        counter--;
-    }, 1000);
-}
-
-// Close the modal if clicked outside the content
-window.onclick = function(event) {
-    if (event.target === modal) {
-        modal.style.display = "none";
-    }
-};
